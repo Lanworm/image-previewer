e01bd540910c1425f3651f1e68c77d6289bd2930
anton.goloschapov <anton.goloschapov@rtlabs.ru>
2024-07-16 14:32:17 +0300

    fix(remarks) конфигурация для интеграционного теста

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 BIN := "./bin/previewer"
 DOCKER_IMG="previewer:develop"
+NGINX_IMAGE_NAME := "nginx-image"
+CONTAINER_NAME := "nginx-container"
+PORT := 8080
 
 GIT_HASH := $(shell git log --format="%h" -n 1)
 LDFLAGS := -X main.release="develop" -X main.buildDate=$(shell date -u +%Y-%m-%dT%H:%M:%S) -X main.gitHash=$(GIT_HASH)
@@ -30,5 +33,23 @@ install-lint-deps:
 
 lint: install-lint-deps
 	golangci-lint run ./...
+
+build-nginx-img:
+	docker build \
+    		-t $(NGINX_IMAGE_NAME) \
+    		-f int_test/Dockerfile .
+
+run-nginx-img: build-nginx-img
+	 docker run -d -p $(PORT):80 --name $(CONTAINER_NAME) $(NGINX_IMAGE_NAME)
+
+stop-nginx-img:
+	docker stop $(CONTAINER_NAME)
+	docker rm $(CONTAINER_NAME)
+
+
+run-int-test:
+	go test ./int_test/... -v -port=$(PORT)
+
+int_test: build-nginx-img run-nginx-img run-int-test stop-nginx-img
 
 .PHONY: build run build-img run-img version test lint
---
+++ int_test/int_test.go
@@ -0,0 +1,46 @@
+package int_test
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+var Port = flag.String("port", "8080", "Container port")
+
+// Функция для проверки наличия изображения по URL
+func checkImagePresence(imagePath string) bool {
+	fmt.Println(Port)
+	baseURL := "http://localhost:" + *Port + "/images/"
+	response, err := http.Get(baseURL + imagePath)
+	if err != nil {
+		return false
+	}
+	defer response.Body.Close()
+	return response.StatusCode == http.StatusOK
+}
+
+// Тестирование различных сценариев
+func TestImageExistence(t *testing.T) {
+	imagePath := "image1.jpg"
+	if !checkImagePresence(imagePath) {
+		t.Errorf("Expected image %s to exist, but it was not found", imagePath)
+	}
+}
+
+func TestImageNotFound(t *testing.T) {
+	imagePath := "non_existent_image.jpg"
+	if checkImagePresence(imagePath) {
+		t.Errorf("Expected image %s to not exist, but it was found", imagePath)
+	}
+}
+
+func TestNonImageFile(t *testing.T) {
+	imagePath := "not_an_image.exe"
+	if checkImagePresence(imagePath) {
+		t.Errorf("Expected %s to not be an image, but it was found", imagePath)
+	}
+}
+
+// Дополнительные тесты для других сценариев...
