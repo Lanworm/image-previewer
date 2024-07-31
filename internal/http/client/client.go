d6bd7bcc89a6f38dbc2e203aaed9bed3d20d0ed6
anton.goloschapov <anton.goloschapov@rtlabs.ru>
2024-07-31 09:09:14 +0300

    feat(tests) убрал мусор

--- internal/http/client/client.go
+++ internal/http/client/client.go
@@ -44,35 +44,3 @@ func (c *Client) DoRequest(method string, url string, body io.Reader, headers ht
 
 	return resp, nil
 }
-
-// func main() {
-//	// Создаем новый HTTP клиент с таймаутом 10 секунд
-//	client := NewHTTPClient(10 * time.Second)
-//
-//	// Создаем контекст с таймаутом 5 секунд
-//	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-//	defer cancel()
-//
-//	// Пример исходного запроса, чтобы получить заголовки
-//	url := "https://jsonplaceholder.typicode.com/posts/1"
-//	origReq, err := http.NewRequest("GET", url, nil)
-//	if err != nil {
-//		fmt.Println("Ошибка создания исходного запроса:", err)
-//		return
-//	}
-//	origReq.Header.Add("Content-Type", "application/json")
-//	origReq.Header.Add("Authorization", "Bearer some_token")
-//
-//	// Конвертируем заголовки из исходного запроса в map[string]string
-//	headers := convertHeaders(origReq.Header)
-//
-//	// Выполняем новый запрос с заголовками из исходного запроса
-//	response, err := client.DoRequest(ctx, "GET", url, nil, headers)
-//	if err != nil {
-//		fmt.Println("Ошибка выполнения запроса:", err)
-//	} else {
-//		defer response.Body.Close()
-//		body, _ := io.ReadAll(response.Body)
-//		fmt.Println("Ответ на GET запрос:", string(body))
-//	}
-//}
